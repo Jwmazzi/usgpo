b69a097eb589f4d1854bfaf33e105a4b0974e437
Jeffrey Scarmazzi <jeff8977@esri.com>
2020-07-05 18:52:37 -0400

    V2 - First Major Revision

--- usgpo/extractor.py
+++ usgpo/extractor.py
@@ -5,6 +5,7 @@ from arcgis.gis import GIS
 
 from datetime import datetime, timedelta
 import xml.etree.ElementTree as et
+from itertools import chain
 import pandas as pd
 import requests
 import json
@@ -78,122 +79,146 @@ class Extractor(object):
 
         return packages
 
-    def process_status(self, status_link):
+    def parse_cosponsor_xml(self, sponsor_element):
 
+        # TODO - Add Parameter to Specify Sponsor or Cosponsor (Maybe Just Have Original Cosponsor Flag)
+
+        return {
+            'full_name': sponsor_element.find('fullName').text,
+            'original_cosponsor': sponsor_element.find('isOriginalCosponsor').text,
+            'sponsor_date': sponsor_element.find('sponsorshipDate').text,
+            'bio_id': f'https://bioguideretro.congress.gov/Home/MemberDetails?memIndex={sponsor_element.find("bioguideId").text}',
+            'party': sponsor_element.find('party').text,
+            'state': sponsor_element.find('state').text
+        }
+
+    def parse_sponsor_xml(self, sponsor_element, introduced_date):
+
+        # TODO - Add Parameter to Specify Sponsor or Cosponsor (Maybe Just Have Original Cosponsor Flag)
+
+        return {
+            'full_name': sponsor_element.find('fullName').text,
+            'original_cosponsor': 'True',
+            'sponsor_date': introduced_date,
+            'bio_id': f'https://bioguideretro.congress.gov/Home/MemberDetails?memIndex={sponsor_element.find("bioguideId").text}',
+            'party': sponsor_element.find('party').text,
+            'state': sponsor_element.find('state').text
+        }
+
+
+    def process_bill_status(self, status_link):
+
+        # TODO - Replace Next with Find Throughout This Function
+        #      - et.fronstring(respon.content).find('bill) Need to be How We Define "Root"
+
+        # Fetch Bill Status XML & Parse
         resp = requests.get(status_link, params={'api_key': self.config["api_key"]})
         root = et.fromstring(resp.content)
 
-        subjects = next(next(next(root.iter('subjects')).iter('billSubjects')).iter('legislativeSubjects'))
-        latest   = next(root.iter('latestAction'))
+        introduced  = next(root.iter('introducedDate')).text
 
-        policy   = next(root.iter('policyArea'))
-        policy   = policy[0].text if len(policy) == 1 else None
+        subjects    = next(next(next(root.iter('subjects')).iter('billSubjects')).iter('legislativeSubjects'))
+        subject_str = ', '.join([i[0].text for i in subjects.iter('item')])
 
-        sub_list = ', '.join([i[0].text for i in subjects.iter('item')])
-        action   = next(latest.iter('text')).text
-        date     = next(latest.iter('actionDate')).text
+        policy      = next(root.iter('policyArea'))
+        policy_area = policy[0].text if len(policy) == 1 else None
 
-        return [sub_list, policy, action, date]
+        latest             = root.find('bill').find('latestAction')
+        latest_action      = next(latest.iter('text')).text
+        latest_action_date = next(latest.iter('actionDate')).text
 
-    def process_package(self, package, keyword):
+        sponsors = []
+        for sponsor in next(root.iter('cosponsors')).iter('item'):
+            sponsor = self.parse_cosponsor_xml(sponsor)
+            sponsor.update({
+                'last_date':   latest_action_date,
+                'last_action': latest_action,
+                'policy_area': policy_area,
+                'subjects':    subject_str[:999],
+            })
+            sponsors.append(sponsor)
+
+        first_sponsor = self.parse_sponsor_xml(next(root.iter('sponsors')).find('item'), introduced)
+        first_sponsor.update({
+            'last_date': latest_action_date,
+            'last_action': latest_action,
+            'policy_area': policy_area,
+            'subjects': subject_str[:999],
+        })
+        sponsors.append(first_sponsor)
+
+        return sponsors
+
+    def process_package(self, package, sponsor_list):
 
         api_key = self.config["api_key"]
+        resp    = requests.get(package.get('packageLink'), params={'api_key': api_key}).json()
 
-        r = requests.get(package.get('packageLink'), params={'api_key': api_key}).json()
-
-        member_list = r.get('members', [])
-
-        subject_list, policy, latest_action, latest_date = self.process_status(r['related']['billStatusLink'])
-
-        for member in member_list:
-            member.update({
-                'link': f"{r['download']['pdfLink']}?api_key={api_key}",
-                'title': r['title'][:999],
-                'package_id': r['packageId'],
-                'bill_number': r['billNumber'],
-                'keyword': keyword,
-                'dateIssued': package['dateIssued'],
-                'committees': ', '.join([c['committeeName'] for c in r.get('committees', [])])[:999],
-                'other_title': ', '.join([c['title'] for c in r.get('shortTitle', [])])[:999],
-                'subjects':  subject_list[:999],
-                'policy_area': policy,
-                'last_action': latest_action,
-                'last_date': latest_date
+        for sponsor in sponsor_list:
+            sponsor.update({
+                'committees': ', '.join([c['committeeName'] for c in resp.get('committees', [])])[:999],
+                'other_title': ', '.join([c['title'] for c in resp.get('shortTitle', [])])[:999],
+                'link': f"{resp['download']['pdfLink']}?api_key={api_key}",
+                'date_issued': package['dateIssued'],
+                'bill_number': resp['billNumber'],
+                'package_id': resp['packageId'],
+                'title': resp['title'][:999]
             })
 
-        return member_list
+        return sponsor_list
 
-    def get_collection_df(self, collection, category, keywords):
+    def get_collection_df(self, collection):
 
         data = []
 
         for package in collection:
-            try:
-                for keyword in [k.lower() for k in keywords]:
-                    if keyword in package['title'].lower():
-                        members = self.process_package(package, keyword)
-                        for member in members:
-                            data.append(member)
-            except KeyError as key_err:
-                print(f'Dropped Record Expecting Key: {key_err}')
+
+            r = requests.get(package.get('packageLink'), params={'api_key': self.config['api_key']}).json()
+
+            sponsor_list = self.process_bill_status(r['related']['billStatusLink'])
+            sponsor_list = self.process_package(package, sponsor_list)
+
+            data.append(sponsor_list)
 
         if not data:
             return pd.DataFrame()
+        else:
+            data = list(chain(*data))
 
         df = pd.DataFrame(data)
 
+        df['sponsor_date'] = pd.to_datetime(df['sponsor_date'])
+        df['date_issued'] = pd.to_datetime(df['date_issued'])
         df['last_date'] = pd.to_datetime(df['last_date'])
-        df['title'] = df['title'].apply(lambda x: f'{x[:250]}')
-        df['category'] = category
 
         return df
 
-    def fetch_terms(self):
-
-        category_itm = self.gis.content.get(self.config['categ_id'])
-        category_fs  = category_itm.tables[0].query().features
-        keyword_fs   = category_itm.tables[1].query().features
-
-        categories = {k: [] for k in [c.attributes['category'] for c in category_fs]}
-
-        for feature in keyword_fs:
-            categories[feature.attributes['category']].append(feature.attributes['keyword'])
-
-        return categories
-
-    def fetch_bills(self, term_dictionary, past_days):
-
-        print(f'Fetching Bill Data for {len(term_dictionary.keys())} Categories')
+    def fetch_bills(self, past_days):
 
         bill_dfs = []
         bill_fts = []
         bill_ids = []
 
-        for category, keywords in term_dictionary.items():
-            for bill_type, bill_desc in bill_types.items():
+        for bill_type, bill_desc in bill_types.items():
 
-                past_time  = self.get_past_time(past_days)
-                collection = self.get_collection('BILLS', past_time, bill_type)
-                collect_df = self.get_collection_df(collection, category, keywords)
+            past_time  = self.get_past_time(past_days)
+            collection = self.get_collection('BILLS', past_time, bill_type)
+            collect_df = self.get_collection_df(collection)
 
-                if len(collect_df) > 0:
+            if len(collect_df) > 0:
 
-                    collect_df['bill_type'] = bill_desc
+                collect_df['bill_type'] = bill_desc
 
-                    for bill_number in collect_df['bill_number'].unique():
-                        if bill_number not in bill_ids:
-                            bills   = collect_df[collect_df['bill_number'] == bill_number]
-                            sponsor = bills[bills['role'] == 'SPONSOR'].spatial.to_featureset().features
-                            if len(sponsor) == 1:
-                                bill_fts.append(sponsor[0])
+                for bill_number in collect_df['bill_number'].unique():
+                    if bill_number not in bill_ids:
+                        bills   = collect_df[collect_df['bill_number'] == bill_number]
+                        bill_fts.append(bills.spatial.to_featureset().features[0])
 
-                    bill_dfs.append(collect_df)
+                bill_dfs.append(collect_df)
 
         return bill_dfs, bill_fts
 
     def process_edits(self, bills_df, bills_fs):
-
-        print('Pushing Edits')
 
         bills_itm = self.gis.content.get(self.config["bills_id"])
         bills_lyr = bills_itm.tables[0]
@@ -217,10 +242,13 @@ class Extractor(object):
         member_df = concat_df.merge(state_df, left_on='state', right_on='STATE_ABBR')
 
         print('Pushing Member Edits')
-        for feature in member_df.spatial.to_featureset().features:
-            response = membs_lyr.edit_features(adds=[feature])['addResults'][0]
-            if not response['success']:
-                print(f'Edit Failed: {response}')
+        member_sets = self.batches(member_df.spatial.to_featureset().features, 500)
+        for idx, member_set in enumerate(member_sets):
+            try:
+                response = membs_lyr.edit_features(adds=member_set)['addResults']
+                print(f'{len([e for e in response if e["success"]])} Edits Passed')
+            except:
+                print(f'Edit Batch {idx} Failed')
 
     def run_solution(self, past_days=30):
 
@@ -228,9 +256,9 @@ class Extractor(object):
 
         self.set_gis()
 
-        term_dictionary  = self.fetch_terms()
-        bill_df, bill_fs = self.fetch_bills(term_dictionary, past_days)
+        bill_df, bill_fs = self.fetch_bills(past_days)
 
         self.process_edits(bill_df, bill_fs)
 
         print(f'Process Ran in {round((time.time() - start) / 60, 2)} Minutes')
+
